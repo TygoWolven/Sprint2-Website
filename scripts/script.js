9afcfafaa7b0609edab542c010885131f1d8f743
TygoWolven <144010858+TygoWolven@users.noreply.github.com>
2023-10-09 11:52:11 +0200

    .

--- scripts/script.js
+++ scripts/script.js
@@ -4,23 +4,27 @@ toggle = body.querySelector(".toggle"),
 searchBtn = body.querySelector(".search-box"),
 modeSwitch = body.querySelector(".toggle-switch"),
 modeText = body.querySelector(".mode-text");
+menuButton = body.querySelector("button");
 
-
-toggle.addEventListener("click" , () =>{
-    sidebar.classList.toggle("close");
+menuButton.addEventListener("click" , () =>{
+    sidebar.classList.toggle("moving");
 })
 
-searchBtn.addEventListener("click" , () =>{
-    sidebar.classList.remove("close");
-})
+// toggle.addEventListener("click" , () =>{
+//     sidebar.classList.toggle("close");
+// })
 
-modeSwitch.addEventListener("click" , () =>{
-    body.classList.toggle("dark");
+// searchBtn.addEventListener("click" , () =>{
+//     sidebar.classList.remove("close");
+// })
+
+// modeSwitch.addEventListener("click" , () =>{
+//     body.classList.toggle("dark");
     
-    if(body.classList.contains("dark")){
-        modeText.innerText = "Light mode";
-    }else{
-        modeText.innerText = "Dark mode";
+//     if(body.classList.contains("dark")){
+//         modeText.innerText = "Light mode";
+//     }else{
+//         modeText.innerText = "Dark mode";
         
-    }
-});
+//     }
+// });
